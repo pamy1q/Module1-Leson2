c4263e169da637a8ec61af12db0d4394a0eae363
pamy1q <britneyojos@gmail.com>
2024-09-03 14:36:19 +0100

    Intro to strings in print

---
+++ main.py
@@ -0,0 +1,4 @@
+print("Welcome to the world of programming")
+print(10)
+print("Hello",5)
+print("Hello,Britney",end=" *")
---
+++ variable.py
@@ -0,0 +1,7 @@
+num=20
+num2="Britney"
+print(num)
+print(num2)
+name = input("Please enter your name:")
+print(f"Hello {name} how are you?")
+print("hello "+ name + " how are you?\n"+ str(num) + "\n"+ num2 )
